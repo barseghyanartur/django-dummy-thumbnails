1df1f906e0e7453a0894263da860812d2270ab3f
Artur Barseghyan <artur.barseghyan@gmail.com>
2016-12-19 00:07:48 +0100

    prepare 0.1.5

--- src/dummy_thumbnails/__init__.py
+++ src/dummy_thumbnails/__init__.py
@@ -5,7 +5,7 @@ Dummy thumbnails for most popular Django thumbnail generators.
 """
 
 __title__ = 'dummy_thumbnails'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2016 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
